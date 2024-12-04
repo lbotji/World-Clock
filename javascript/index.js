e86316a9a63e5e8b764239dc103597aca235d555
Lehlohonlo Botji <lbotji@gmail.com>
2024-12-04 16:30:53 +0200

    removed fake data from html file. Injected time and date data to javascript from moment cdn library

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
-alert(Hello)
+setInterval(function() {
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDatesElement = losAngelesElement.querySelector(".dates");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz("America/Los_Angeles");
+losAngelesDatesElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement = sydneyElement.querySelector(".time");
+let sydneyTime = moment().tz("Australia/Sydney");
+sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
+
+}, 1000);
